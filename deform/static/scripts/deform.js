9911d8313cb9459f3ffee5c26078fdd732a40f6f
Chris McDonough <chrism@plope.com>
2010-03-24 19:22:39 +0000

    Get object insertion ordering more right.

--- deform/static/scripts/deform.js
+++ deform/static/scripts/deform.js
@@ -1,8 +1,21 @@
+function last_sibling(node) {
+    var tempObj=node.parentNode.lastChild;
+    // nodeType 1 == element
+    while(tempObj.nodeType != 1 && tempObj.previousSibling != null) {
+        tempObj=tempObj.previousSibling;
+    }
+    return (tempObj.nodeType == 1) ? tempObj: false;
+}
+
+function insert_after(node, target) {
+    target.parentNode.insertBefore(node, target);
+}
+    
 function add_new_item(source) {
     var protonode = source.previousElementSibling;
     var code = protonode.attributes['prototype'].value;
     var html = decodeURIComponent(code);
-    node = document.createElement('div');
+    var node = document.createElement('div');
     node.innerHTML = html;
-    source.parentNode.insertBefore(node.firstChild, source.nextSibling);
+    insert_after(node.firstChild, last_sibling(source));
 }
