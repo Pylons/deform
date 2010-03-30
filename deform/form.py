c35864baaab4113607466fb139b184643e71353d
Chris McDonough <chrism@plope.com>
2010-03-30 22:38:20 +0000

    Docstring.

--- deform/form.py
+++ deform/form.py
@@ -10,6 +10,13 @@ from deform import widget
 class Field(object):
     """ Represents an individual form field (a visible object in a
     form rendering).
+
+    A :class:`deform.form.Field` object instance is meant to last for
+    the duration of a single web request. A field may be used as a
+    scratchpad by the widget associated with the field; this makes it
+    possible to build implementations of a widget which needs a place
+    to hold state need while instances of the widget still only need
+    to be constructed once instead of on each request.
     
     All field objects have the following attributes:
 
