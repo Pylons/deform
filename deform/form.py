ecc144ef80bcc1f9da0ad03b7a4501fb526c042b
Chris McDonough <chrism@plope.com>
2010-05-13 03:51:50 +0000

    - Every form has a formid now, defaulting to ``deform``.  The formid   is used to compute the id of the form tag as well as the button ids   in the form.  Previously, if a formid was not passed to the Form   constructor, no id would be given to the rendered form and the   buttons would not be prefixed with any formid.

--- deform/form.py
+++ deform/form.py
@@ -28,17 +28,15 @@ class Form(field.Field):
         :class:`deform.form.Button` objects.
 
     formid
-        The identifier for this form.  This value, if not ``None``,
-        will be used as the value of a hidden form input control
-        (``__formid__``) which will be placed in this form's
-        rendering.  You should pass a non-``None`` value for
-        ``formid`` when more than one Deform form is placed into a
-        single page and both share the same action.  When one of the
-        forms on the page is posted, your code will to be able to
-        decide which of those forms was posted based on the value of
-        ``__formid__``.  If this value is ``None``, a hidden
-        ``__formid__`` control will not be placed into the form's
-        rendering.
+        The identifier for this form.  This value will be used as the
+        value of a hidden form input control (``__formid__``) which
+        will be placed in this form's rendering.  You should pass a
+        string value for ``formid`` when more than one Deform form is
+        placed into a single page and both share the same action.
+        When one of the forms on the page is posted, your code will to
+        be able to decide which of those forms was posted based on the
+        differing values of ``__formid__``.  By default, ``formid`` is
+        ``deform``.
 
     The :class:`deform.Form` constructor also accepts all the keyword
     arguments accepted by the :class`deform.Field` class.  These
@@ -46,7 +44,7 @@ class Form(field.Field):
     in the context of a Field.
     """
     def __init__(self, schema, action='.', method='POST', buttons=(),
-                 formid=None, **kw):
+                 formid='deform', **kw):
         field.Field.__init__(self, schema, **kw)
         _buttons = []
         for button in buttons:
@@ -93,4 +91,3 @@ class Button(object):
         self.name = name
         self.title = title
         self.value = value
-
