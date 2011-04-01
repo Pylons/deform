d26e2dcd5ef51c635f8f752af6019a0099a437b9
Fabian Neumann <fn@sturzbach.de>
2011-04-01 20:18:56 +0200

    Refactor to valid HTML and upgrade to latest jQuery + libs.
    Instead of using custom (non-valid) attributes (e.g. prototype) and
    a (non-valid) SPAN inside a sequence's UL to hold min/max variables,
    we use HTML5 data-* attributes now.
    
    For easy access to them (and for the hotness' sake) upgrade to
    jQuery 1.5.2 and a more recent jQuery UI.

--- deform/widget.py
+++ deform/widget.py
@@ -1427,26 +1427,26 @@ class ResourceRegistry(object):
 default_resources = {
     'jquery': {
         None:{
-            'js':'scripts/jquery-1.4.2.min.js',
+            'js':'scripts/jquery-1.5.2.min.js',
             },
         },
     'jqueryui': {
         None:{
-            'js':('scripts/jquery-1.4.2.min.js',
+            'js':('scripts/jquery-1.5.2.min.js',
                   'scripts/jquery-ui-1.8.11.custom.min.js'),
             'css':'css/ui-lightness/jquery-ui-1.8.11.custom.css',
             },
         },
     'jquery.form': {
         None:{
-            'js':('scripts/jquery-1.4.2.min.js',
+            'js':('scripts/jquery-1.5.2.min.js',
                   'scripts/jquery.form.js'),
             },
         },
     'jquery.maskedinput': {
         None:{
-            'js':('scripts/jquery-1.4.2.min.js',
-                  'scripts/jquery.maskedinput-1.2.2.min.js'),
+            'js':('scripts/jquery-1.5.2.min.js',
+                  'scripts/jquery.maskedinput-1.3.min.js'),
             },
         },
     'datetimepicker': {
@@ -1458,7 +1458,7 @@ default_resources = {
         },
     'deform': {
         None:{
-            'js':('scripts/jquery-1.4.2.min.js',
+            'js':('scripts/jquery-1.5.2.min.js',
                   'scripts/jquery.form.js',
                   'scripts/deform.js'),
             'css':('css/form.css')
