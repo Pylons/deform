3d79c4783d1cd0ab286b9933ec52b0d5159f2da2
Chris Withers <chris@simplistix.co.uk>
2011-05-22 23:27:56 +0100

    use intersphinx for better cross-package references

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,15 @@ for item in os.listdir(parent):
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    ]
+
+intersphinx_mapping = dict(
+    python=('http://docs.python.org/dev',None),
+    colander=('http://docs.pylonsproject.org/projects/colander/dev/',None)
+    )
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
