Pylons/deform

commit 07b09e2171f9c50f0a8054b0d440ace25b40413f

- The various deform* JavaScript functions in ``deform.js`` have now   been moved into a top-level namespace.  For example, where it was   necessary to call ``deformFocusFirstInput()`` before, it is now   necessary to call ``deform.focusFirstInput()``.
- Make the TinyMCE rich text widget use ``mode: 'exact'`` instead of
  ``mode: 'textareas'``.

- Change sequence adding logic to be slightly simpler.

deform/static/scripts/deform.js
@@ -1,67 +1,64 @@
-function deformAddNewItem(protonode, before) {
-    // - Clone the prototype node and add it before the "before" node.
+var deform  = {
 
-    // In order to avoid breaking accessibility:
-    //
-    // - Find all the label tags within the prototype node.
-    // - For each label referencing an id, find the node with that id.
-    // - Change the label reference and the node id to a random string. 
+    addSequenceItem: function (protonode, before) {
+        // - Clone the prototype node and add it before the "before" node.
 
-    var code = protonode.attributes['prototype'].value;
-    var html = decodeURIComponent(code);
-    var $node = $(html);
-    var $labels = $node.find('label');
-    var labeled = {};
+        // In order to avoid breaking accessibility:
+        //
+        // - Find each tag within the prototype node with an id
+        //   that has the string ``deformField(\d+)`` within it, and modify 
+        //   its id to have a random component.
+        // - For each label referencing an change id, change the label's
+        //   htmlFor attribute to the new id.
 
-    // find all labels for each id
-    for (var i = 0; i < $labels.length; i++) {
-        var label = $labels.get(i);
-        var labelid = label.htmlFor;
-        if (labelid) {
-            var tmp = labeled[labelid] || [];
-            tmp.push(label);
-            labeled[labelid] = tmp;
+        var fieldmatch = /deformField(\d+)/;
+        var code = protonode.attributes['prototype'].value;
+        var html = decodeURIComponent(code);
+        var $htmlnode = $(html);
+        var $idnodes = $htmlnode.find('[id]');
+        var genid = deform.randomString(6);
+
+        $idnodes.each(function(idx, node) {
+            var $node = $(node);
+            var oldid = $node.attr('id');
+            var newid = oldid.replace(fieldmatch, "deformField$1-" + genid);
+            $node.attr('id', newid);
+
+            var labelselector = 'label[htmlFor=' + oldid + ']';
+            var $fornodes = $htmlnode.find(labelselector);
+            $fornodes.attr('htmlFor', newid);
+            });
+
+        $htmlnode.insertBefore(before);
+    },
+
+    focusFirstInput: function () {
+        var input = $(':input').filter('[id ^= deformField]').first();
+        if (input) {
+            var raw = input.get(0);
+            if (raw) {
+                if (raw.type === 'text' || raw.type === 'file' || 
+                    raw.type == 'password' || raw.type == 'text' || 
+                    raw.type == 'textarea') { 
+                    input.focus();
+                };
+            };
         };
-    };
+    },
 
-    // for each labelid, find the node it points to and replace its id
-    // with a generated id; also set the for= attribute of the labels
-    // which reference to the same generated id
-    for (labelid in labeled) {
-        var labels = labeled[labelid];
-        var genid = randomString(10);
-        $node.find('#' + labelid).attr('id', genid);
-        for (var i = 0; i < labels.length; i++) {
-            var label = labels[i];
-            label.htmlFor = genid;
-        };
-    };
-    $node.insertBefore(before);
-}
-
-function randomString(length) {
-    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
-    var chars = chars.split('');
+    randomString: function (length) {
+        var chr='0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+        var chr = chr.split('');
     
-    if (! length) {
-        length = Math.floor(Math.random() * chars.length);
-    }
-    
-    var str = '';
-    for (var i = 0; i < length; i++) {
-        str += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return str;
-}
-
-function deformFocusFirstInput() {
-    var input = $(':input').filter('[id ^= deformField]').first();
-    if (input) {
-        var raw = input.get(0);
-        if (raw.type === 'text' || raw.type === 'file' || 
-            raw.type == 'password' || raw.type == 'text' || 
-            raw.type == 'textarea') { 
-            input.focus();
+        if (! length) {
+            length = Math.floor(Math.random() * chr.length);
         };
-    };
+    
+        var str = '';
+        for (var i = 0; i < length; i++) {
+            str += chr[Math.floor(Math.random() * chr.length)];
+        };
+        return str;
+    }
+
 };

deform/templates/richtext.pt
@@ -1,15 +1,15 @@
-<textarea tal:attributes="class: string:tinymce;"
-          id="${field.oid}"
+<textarea id="${field.oid}"
           name="${field.name}">${cstruct}</textarea>
 <script language="javascript"
 	type="text/javascript">
   tinyMCE.init({
+    mode : 'exact',
+    elements: '${field.oid}',
     height: '${field.widget.height}',
-    mode : 'textareas',
+    width: '${field.widget.width}',
     theme : '${field.widget.theme}',
     theme_advanced_resizing : true,
     theme_advanced_toolbar_align : 'left',
-    theme_advanced_toolbar_location : 'top',
-    width: '${field.widget.width}'
+    theme_advanced_toolbar_location : 'top'
   });
 </script>

deform/templates/sequence.pt
@@ -11,7 +11,7 @@
      class="deformSeqAdd"
      id="${field.oid}-seqAdd"
      onclick="javascript:
-              deformAddNewItem($(this).parent().children('.deformProto')[0],
+              deform.addSequenceItem($(this).parent().children('.deformProto')[0],
               $(this).parent().children('.deformInsertBefore')[0]);
               return false;">
     <small id="${field.oid}-addtext">${add_subitem_text}</small>

deformdemo/templates/form.pt
@@ -10,7 +10,7 @@
     </h1>
     <span tal:replace="structure code"/>
     <script type="text/javascript">
-      deformFocusFirstInput();
+      deform.focusFirstInput();
     </script>
   </div>
 </div>

deformdemo/templates/translated_form.pt
@@ -17,7 +17,7 @@
     </h1>
     <span tal:replace="structure code"/>
     <script type="text/javascript">
-      deformFocusFirstInput();
+      deform.focusFirstInput();
     </script>
   </div>
 </div>

deformdemo/tests/test_demo.py
@@ -1166,7 +1166,7 @@ class SequenceOfFileUploads(unittest.TestCase):
         browser.wait_for_page_to_load("30000")
         browser.click('deformField1-seqAdd')
         path, filename = _getFile('test_demo.py')
-        browser.type("deformField3", path)
+        browser.type("dom=document.forms[0].upload", path)
         browser.click("submit")
         browser.wait_for_page_to_load("30000")
 
@@ -1185,7 +1185,7 @@ class SequenceOfFileUploads(unittest.TestCase):
         browser.wait_for_page_to_load("30000")
         browser.click('deformField1-seqAdd')
         path, filename = _getFile('test_demo.py')
-        browser.type("deformField3", path)
+        browser.type("dom=document.forms[0].upload", path)
         browser.click("submit")
         browser.wait_for_page_to_load("30000")
 
@@ -1220,7 +1220,7 @@ class SequenceOfFileUploads(unittest.TestCase):
         # add a new file
         browser.click('deformField1-seqAdd')
         path, filename = _getFile('test_demo.py')
-        browser.type("deformField4", path)
+        browser.type("dom=document.forms[0].upload[1]", path)
         browser.click("submit")
         browser.wait_for_page_to_load("30000")
         self.assertEqual(browser.get_text('css=#deformField3-filename'),

commit 039c0820950d1fcd0ddb7366c39c8feeb6ec5c50

Set correct 'for' attribute on label of new seq item
This was broken in 07b09e21 when the code was changed to use jQuery's
API, but the accessor was not changed. It is simply 'for' when using
jQuery and htmlFor on the DOM object.

deform/static/scripts/deform.js
@@ -52,7 +52,7 @@ var deform  = {
         //   that has the string ``deformField(\d+)`` within it, and modify 
         //   its id to have a random component.
         // - For each label referencing an change id, change the label's
-        //   htmlFor attribute to the new id.
+        //   for attribute to the new id.
 
         var fieldmatch = /deformField(\d+)/;
         var namematch = /(.+)?-[#]{3}/;
@@ -73,9 +73,9 @@ var deform  = {
             var newid = oldid.replace(fieldmatch, "deformField$1-" + genid);
             $node.attr('id', newid);
             idmap[oldid] = newid;
-            var labelselector = 'label[htmlFor=' + oldid + ']';
+            var labelselector = 'label[for=' + oldid + ']';
             var $fornodes = $htmlnode.find(labelselector);
-            $fornodes.attr('htmlFor', newid);
+            $fornodes.attr('for', newid);
             });
 
         // replace names a containing ```deformField`` like we do for ids