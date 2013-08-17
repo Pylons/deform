c3e25a7b6c5ed2b5cb8ae05cfa8a21e08ba59401
Balazs Ree <ree@greenfinity.hu>
2013-08-17 06:22:21 +0200

    Add doc

--- deform/resource.py
+++ deform/resource.py
@@ -8,25 +8,48 @@ map_path = os.path.join(
         os.path.dirname(sys.modules[__name__].__file__)),
     'static', 'dist', 'map.json')
 
-def get_with_dependencies(pkg):
+def _get_with_dependencies(pkg):
     js = list(pkg['js'])
     css = list(pkg['css'])
     for dep_pkg in pkg['dependencies']:
-        dep = get_with_dependencies(dep_pkg)
+        dep = _get_with_dependencies(dep_pkg)
         js.extend(dep['js'])
         css.extend(dep['css'])
     return dict(js=js, css=css)
 
-def collect_resources_for_packages(*packages):
+
+def collect_resources_for_widgets(*packages):
+    """Give js and css resources needed to render a list of
+    widgets in the page.
+
+    This method uses the `static/dist/map.json` file to learn about
+    installed widgets and their dependencies. This file is generated
+    by process_dependencies.js based on the installation created
+    by 'bower'. As such, it represents a global state of all installed
+    front-end packages. Since all widgets have a `bower.json` dependency
+    file, they are treated as genuine front-end packages themselves.
+
+    The method takes a list of widget names which are typically in the
+    form of "autocomplete-input-widget", the names correspond to the
+    `bower.json` files created for the widgets. If the packages list are empty,
+    all the widgets are selected. Otherwise only the widgets in the
+    packages list _and_ all their dependencies will be selected.
+
+    The method returns a dictionary with two fields: 'js' and 'css', each
+    containing a list of resources in the order they need to be included
+    from html. The resource path are relative to 'static'.
+    """
     map_data = json.loads(open(map_path).read())
     js = []
     css = []
     for pkg in map_data['dependencies']:
         if not packages or pkg['name'] in packages:
-            dep = get_with_dependencies(pkg)
+            dep = _get_with_dependencies(pkg)
             js.extend(dep['js'])
             css.extend(dep['css'])
-
+    # Reverse the order of resources, so dependencies
+    # come to the front. Also make sure that each resource
+    # is only included once into the list.
     js.reverse()
     unique_js = []
     for item in js:
