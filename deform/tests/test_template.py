bdd9d4cff117b96ce64bf498f491a2b21bbe4032
Domen Kožar <domen@dev.si>
2013-09-12 01:26:00 +0200

    fix tests

--- deform/tests/test_template.py
+++ deform/tests/test_template.py
@@ -115,7 +115,7 @@ class Test_default_renderer(unittest.TestCase):
         result = result.strip()
         self.assertEqual(
             result,
-            text_type('<label for="oid" > <input type="checkbox" '
+            text_type('<label for="oid"> <input type="checkbox" '
                       'name="name" value="true" id="oid"/> </label>')
             )
 
