b13bfddf06273e39cd602190781cb8ccdb0f8135
Tres Seaver <tseaver@palladion.com>
2014-12-12 10:35:50 -0500

    Add support for Python 3.4, PyPy3.

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setupkw = dict(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         ],
