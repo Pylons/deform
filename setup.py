c7764dd2617f530e1776d5eff91fae302dd2a6ef
Mikko Ohtamaa <mikko@opensourcehacker.com>
2016-10-22 15:12:01 -0400

    Back to development: 2.0b2

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ docs_extras = [
 
 setupkw = dict(
     name='deform',
-    version='2.0b1',
+    version='2.0b2.dev0',
     description='Form library with advanced features like nested forms',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
