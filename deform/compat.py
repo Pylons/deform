9b2b1f2df7f3f1d39d0c42914aa46018cd916050
Steve Piercy <web@stevepiercy.com>
2020-05-01 02:18:45 -0700

    Toggle coverage from Python 2 to Python 3 block

--- deform/compat.py
+++ deform/compat.py
@@ -15,14 +15,14 @@ except AttributeError:  # pragma: no cover
 # True if we are running on Python 3.
 PY3 = sys.version_info[0] == 3
 
-if PY3:  # pragma: no cover
+if PY3:
     string_types = (str,)
     integer_types = (int,)
     class_types = (type,)
     text_type = str
     binary_type = bytes
     long = int
-else:
+else:  # pragma: no cover
     string_types = (basestring,)
     integer_types = (int, long)
     class_types = (type, types.ClassType)
