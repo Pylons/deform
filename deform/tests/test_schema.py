608b2fbe170b1e10bfef0d6ff60d6c94ca3dbf32
Sydo Luciani <60237469+sydoluciani@users.noreply.github.com>
2020-05-15 18:38:55 -0500

    Replaced MagicMock with DummyRequest.

--- deform/tests/test_schema.py
+++ deform/tests/test_schema.py
@@ -21,20 +21,17 @@ class TestFileData(unittest.TestCase):
         return FileData()
 
     def test_deferred_csrf_value(self):
-        import colander
+        from pyramid import testing
         from deform.schema import CSRFSchema
-        from unittest.mock import MagicMock
         from deform.schema import deferred_csrf_value
 
-        kw = {"request": MagicMock(name="request")}
-        kw["request"].session = MagicMock(name="session")
-        kw["request"].session.get_csrf_token = MagicMock(
-            return_value=colander.null)
-
+        new_request = testing.DummyRequest()
+        kw = {"request": new_request}
         node = DummySchemaNode()
         new_schema = CSRFSchema()
+        token = new_request.session.get_csrf_token()
         new_schema.default = deferred_csrf_value(node, kw)
-        self.assertEqual(new_schema.default, colander.null)
+        self.assertEqual(new_schema.default, token)
 
     def test_deserialize_null(self):
         from colander import null
